4684ad837e8c602680a856124abcf5f89be60cca
Kevin Leturc <kleturc@nuxeo.com>
2021-03-22 17:19:27 +0100

    NXP-30179: Add a script to download/upload Libreoffice

---
+++ Jenkinsfiles/libreoffice.groovy
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2021 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Kevin Leturc <kleturc@nuxeo.com>
+ */
+
+properties([
+  [$class: 'GithubProjectProperty', projectUrlStr: 'https://github.com/nuxeo/platform-ci-tools/'],
+  [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', daysToKeepStr: '60', numToKeepStr: '60', artifactNumToKeepStr: '5']],
+  disableConcurrentBuilds(),
+])
+
+pipeline {
+  agent {
+    label 'jenkins-jx-base'
+  }
+  parameters {
+    string(name: 'LIBREOFFICE_VERSION', defaultValue: '7.1.1', description: 'Libreoffice version to download from documentfoundation.org and then to upload to packages.nuxeo.com.')
+  }
+  environment {
+    DOCUMENT_FOUNDATION_REPOSITORY = 'https://packages.nuxeo.com/repository/document-foundation-raw'
+
+    LIBREOFFICE_VERSION = "${params.LIBREOFFICE_VERSION}"
+    LIBREOFFICE_TARBALL = "LibreOffice_${LIBREOFFICE_VERSION}_Linux_x86-64_rpm.tar.gz"
+  }
+  stages {
+    stage('Download and upload Libreoffice') {
+      steps {
+        container('jx-base') {
+          echo """
+          ------------------------------------------------
+          Download Libreoffice from documentfoundation.org
+          ------------------------------------------------"""
+          sh 'curl --fail -L https://download.documentfoundation.org/libreoffice/stable/$LIBREOFFICE_VERSION/rpm/x86_64/$LIBREOFFICE_TARBALL --output $LIBREOFFICE_TARBALL'
+
+          echo """
+          ------------------------------------------------
+          Upload Libreoffice to packages.nuxeo.com
+          ------------------------------------------------"""
+          withCredentials([usernameColonPassword(credentialsId: 'packages.nuxeo.com-auth', variable: 'PACKAGES_PASS')]) {
+            sh 'curl -i --fail -u $PACKAGES_PASS --upload-file $LIBREOFFICE_TARBALL "$DOCUMENT_FOUNDATION_REPOSITORY/$LIBREOFFICE_TARBALL"'
+          }
+        }
+      }
+    }
+  }
+}
