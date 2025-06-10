996958b6bdbfbb672e6e6d14f0699d75266eea60
Kevin Leturc <kevinleturc@users.noreply.github.com>
2025-06-10 16:50:59 +0200

    NXBT-3879: Set status to parent job if child job receives hidden parameters

--- Jenkinsfiles/nev/deploy-preview.groovy
+++ Jenkinsfiles/nev/deploy-preview.groovy
@@ -16,7 +16,7 @@
  * Contributors:
  *     Antoine Taillefer <antoine.taillefer@hyland.com>
  */
-library identifier: "platform-ci-shared-library@v0.0.42"
+library identifier: "platform-ci-shared-library@v0.0.68"
 
 String getLatestPackageVersion(name, ltsVersion) {
   withCredentials([usernameColonPassword(credentialsId: 'connect-prod', variable: 'CONNECT_AUTH')]) {
